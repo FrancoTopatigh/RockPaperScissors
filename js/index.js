a64075adafd0bc5ddd35804008cbb75af522c020
Franco Topatigh <topatighfranco@gmail.com>
2025-04-03 21:18:22 -0300

    UI added

--- js/index.js
+++ js/index.js
@@ -1,92 +1,132 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const resultsDiv = document.createElement("div");
+document.body.appendChild(resultsDiv);
+
+const selectionHumanChoice = document.createElement("div");
+const selectionComputerChoice = document.createElement("div");
+const partialResults = document.createElement("div");
+const finalResult = document.createElement("div");
+
+resultsDiv.appendChild(selectionHumanChoice);
+resultsDiv.appendChild(selectionComputerChoice);
+resultsDiv.appendChild(partialResults);
+resultsDiv.appendChild(finalResult);
+
+const optionsArray = ["Rock", "Paper", "Scissors"];
+
 function getRandomInt(){
-    return Math.floor(Math.random() * 3) + 1;
+    return Math.floor(Math.random() * optionsArray.length);
 }
 
 function getComputerChoice(){
-    let computerChoice = getRandomInt();
-    switch(computerChoice){
-        case 1: 
-            console.log("Computer chose: Rock");
-            return 1;
-        case 2:
-            console.log("Computer chose: Paper");
-            return 2;
-        case 3:
-            console.log("Computer chose: Scissors");
-            return 3;
-    }
+    let computerChoice = optionsArray[getRandomInt()];
+    console.log(`Computer chose ${computerChoice}`);
+    return computerChoice;
 }
 
-function getHumanChoice(){
-    let promptHumanChoice = Number(prompt("Choose an option: 1. Rock \n 2.Paper \n 3.Scissors"));
-
-    if(promptHumanChoice < 1 || promptHumanChoice > 3){
-        console.clear();
-        console.log("Choose again");
-        return getHumanChoice();
-    } 
-
-    switch(promptHumanChoice){
-        case 1:
+function getHumanChoice(choice){
+    switch(choice){
+        case "Rock":
             console.log("You chose: Rock");
             break;
-        case 2:
+        case "Paper":
             console.log("You chose: Paper");
             break;
-        case 3:
+        case "Scissors":
             console.log("You chose: Scissors");
             break;
+        default:
+            console.log("Invalid choose");
+            return null;
     }
 
-    return promptHumanChoice;
-
+    return choice;
 }
 
 function playRound(humanChoice, computerChoice){
     if(humanChoice == computerChoice){
         console.log("It's a tie!");
+    } else if (
+        (humanChoice == "Rock" && computerChoice == "Scissors") ||
+        (humanChoice == "Paper" && computerChoice == "Rock")||
+        (humanChoice == "Scissors" && computerChoice == "Paper")
+    ){
+        console.log("Human has won");
+        humanScore++;
+    } else{
+        console.log("Computer has won");
+        computerScore++;
     }
 
-    if(humanChoice == 2 && computerChoice == 1){
-        console.log("Human has won!");
-        humanScore++;
-    } else if (humanChoice == 3 && (computerChoice == 1 || computerChoice == 2)){
-        console.log("Human has won!");
-        humanScore++;
-    } else if(computerChoice == 2 && humanChoice == 1){
-        console.log("Computer has won!");
-        computerScore++;
-    } else if(computerChoice == 3 && (humanChoice == 1 || humanChoice == 2)){
-        console.log("Computer has won!");
-        computerScore++;
-    }
+    selectionHumanChoice.textContent = `You chose ${humanChoice}`;
+    selectionComputerChoice.textContent = `You chose ${computerChoice}`;
 }
 
 function playGame(){
-    for(i = 1; i <= 5; i++){
-        console.log(`\nRound ${i}`);
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
+
+    if(humanScore == 5){
+        finalResult.textContent = "Human has won!";
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+    } else if (computerScore == 5){
+        finalResult.textContent = "Computer has won";
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+    } else{
+        finalResult.textContent = "It's a tie";
     }
 
-    console.log("\nFinal Score:");
-    console.log(`Human: ${humanScore} - Computer: ${computerScore}`);
+    partialResults.textContent = `Human: ${humanScore} | Computer: ${computerScore}`;
 
-    if(humanScore == computerScore){
-        console.log("It's a tie!");
-    } else if (humanScore > computerScore){
-        console.log("Human has won!")
-    } else if(computerScore > humanScore){
-        console.log("Computer has won!")
-    }
 }
 
 playGame();
 
+
+// NUEVAS FUNCIONALIDADES
+
+
+const divHumanOptions = document.querySelector(".human-options");
+
+const rockBtn = document.createElement("button");
+rockBtn.classList.add("rock-btn");
+rockBtn.style.margin = "10px";
+rockBtn.textContent = "Rock";
+
+const paperBtn = document.createElement("button");
+paperBtn.classList.add("paper-btn");
+paperBtn.style.margin = "10px";
+paperBtn.textContent = "Paper";
+
+const scissorsBtn = document.createElement("button");
+scissorsBtn.classList.add("scissors-btn");
+scissorsBtn.style.margin = "10px";
+scissorsBtn.textContent = "Scissors";
+
+divHumanOptions.appendChild(rockBtn);
+divHumanOptions.appendChild(paperBtn);
+divHumanOptions.appendChild(scissorsBtn);
+
+rockBtn.addEventListener("click", () => {
+    playRound("Rock", getComputerChoice());
+    playGame();
+});
+
+
+paperBtn.addEventListener("click", () => {
+    playRound("Paper", getComputerChoice());
+    playGame();
+});
+
+
+scissorsBtn.addEventListener("click", () => {
+    playRound("Scissors", getComputerChoice());
+    playGame();
+});
 
 
 
